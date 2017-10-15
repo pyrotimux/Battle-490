d5193a9fda98c781736ca963a56f2172de530de2
Phyo W. Shwe <chaosmuskey@gmail.com>
2017-10-15 16:40:59 -0700

    Added network and moveable area.

---
+++ Battle-490/Assets/Resources/Scripts/ProtoContrl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ProtoContrl : NetworkBehaviour {
+    private Vector3 hitpos;
+    private ProtoMove sltobj;
+    public GameObject movarea;
+
+    [SyncVar]
+    public string playerstr = "1110000110000110000";
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            Camera.main.transform.position = this.transform.position;
+            Camera.main.transform.rotation = this.transform.rotation;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name.StartsWith("p1"))
+                {
+                    if (sltobj) sltobj.selected = false;
+                    sltobj = hit.transform.GetComponent<ProtoMove>();
+                    sltobj.selected = true;
+                    GameObject.Instantiate(movarea, new Vector3(sltobj.transform.position.x,-0.57f, sltobj.transform.position.z), Quaternion.identity);
+                }
+                else if(hit.transform.name.StartsWith("moveablearea")) 
+                {
+                    sltobj.moveto = hit.point;
+                    Destroy(hit.transform.gameObject);
+                }
+
+            }
+
+        }
+    }
+}
