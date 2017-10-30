2be9a2621f274893b0ccd10ec9ef02cd7807ec42
muhammad hilmi <hilmihisham@live.com>
2017-10-30 01:34:48 -0700

    add UIButton script
    nothing happens, still..

---
+++ Battle-490/Assets/Scripts/UIButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+
+    //all the buttons on the UI
+    GameObject buttonAll, buttonMove, buttonAttack, buttonDone, buttonEndTurn;
+
+    //initialize all button
+    public void InitButton()
+    {
+        buttonAll = GameObject.Find("UIButtonCtrl");
+        buttonMove = buttonAll.transform.GetChild(1).gameObject;
+        buttonAttack = buttonAll.transform.GetChild(2).gameObject;
+        buttonDone = buttonAll.transform.GetChild(3).gameObject;
+        buttonEndTurn = buttonAll.transform.GetChild(4).gameObject;
+    }
+
+    public void End_Turn_Button_Down()
+    {
+        Input.GetKeyDown("e");
+        Debug.Log("End Turn pressed!");
+        
+    }
+
+    /*
+	// Use this for initialization
+	void Start () {
+		
+	}
+	*/
+     /*
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    */
+    
+}
