71a3d3f701136b49699b8b88fc89213c08c32be1
muhammad hilmi <hilmihisham@live.com>
2017-11-27 00:43:18 -0800

    initial idea of initializing toon's stats
    entah benda ni jalan lagu dia tulih ke dak tu idok le menahunya lagi...

---
+++ Battle-490/Assets/Scripts/ProtoStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class toonDetail
+{
+    public string toonType; //data, virus or antivirus?
+    public int health; //toon HP count
+    public int atkpower; //toon attack power
+    public int defpower; //toon defense power
+}
+
+public class ProtoStats : MonoBehaviour {
+
+    toonDetail toon;
+
+	// Use this for initialization
+	void Start () {
+
+        //initializing each toons stats below
+        toon = new toonDetail();
+        //toon.toonType = "";
+        toon.health = 3;
+        toon.atkpower = 2;
+        toon.defpower = 1;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
