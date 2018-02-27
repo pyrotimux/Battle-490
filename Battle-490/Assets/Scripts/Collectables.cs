1bc0be6fb09133456a556371c3ad30421e160bd0
muhammad hilmi <hilmihisham@live.com>
2018-02-26 21:42:06 -0800

    cleaning up and adding comments on collectables script

--- Battle-490/Assets/Scripts/Collectables.cs
+++ Battle-490/Assets/Scripts/Collectables.cs
@@ -6,12 +6,13 @@ using UnityEngine.Networking;
 
 public class Collectables : NetworkBehaviour {
 
-    public GameObject collectablesEffect;
-    public int increaseScore = 50;
+    public GameObject collectablesEffect; // effect for when any toons hit it
+    public int increaseScore = 50; // score that each collectables carry
     ProtoContrl[] plyrs;
 
     void Start()
     {
+        // delay to 3 to wait for GameMgr (deyaled 2) to prepare all the player first
         StartCoroutine(DelayStart(3)); // delay init.
     }
 
@@ -20,7 +21,7 @@ public class Collectables : NetworkBehaviour {
         yield return new WaitForSeconds(time);
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player"); // get all players from game
         int j = 0; // counter so we can assign them 
-        plyrs = new ProtoContrl[players.Length]; // init array with lenght of total player
+        plyrs = new ProtoContrl[players.Length]; // init array with length of total player
         foreach (GameObject p in players)
         {
             plyrs[j++] = p.GetComponent<ProtoContrl>(); // get all the proto control and save it in array.
@@ -28,12 +29,14 @@ public class Collectables : NetworkBehaviour {
 
     }
 
+    // destroying game object on the network (not just locally)
     [Command]
     void CmdDestroyGameObject(GameObject go)
     {
         NetworkServer.Destroy(go);
     }
 
+    // when other (the toon in this case) collided with the collectables
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("toons"))
@@ -44,31 +47,30 @@ public class Collectables : NetworkBehaviour {
 
     void AddScore(Collider toon)
     {
-        // spawn effect when collide
+        // step 1 : spawn effect when collide
         // (install and use Unity Particle Pack and refer it to collectablesEffect)
         //Instantiate(collectablesEffect, transform.position, transform.rotation);
 
-        // add score
+        // step 2 : add score
         Debug.Log("Crashing into collectables..");
-        String name = toon.GetComponent<ProtoMove>().owner;
+        String name = toon.GetComponent<ProtoMove>().owner; // get the owner of the toon
         int j = 0;
         ProtoContrl ply = null;
 
         foreach (ProtoContrl p in plyrs)
         {
             Debug.Log(plyrs[j].pname + " " + name);
-            if (plyrs[j].pname == name) {
+            if (plyrs[j].pname == name) { // checking the name
                 ply = plyrs[j];
-            }
+            } 
             j++;
                 
         }
 
-        //Debug.Log("Player score : " + score.playerScore);
-        ply.playerScore += increaseScore;
-        Debug.Log("Player new score : " + ply.playerScore);
+        ply.playerScore += increaseScore; // adding score to playerScore in ProtoContrl (total score stored there)
+        Debug.Log(ply.pname + " new score : " + ply.playerScore);
 
-        // remove collectables
+        // step 3 : remove collectables from the game
         CmdDestroyGameObject(gameObject);
     }
 }
